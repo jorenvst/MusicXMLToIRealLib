5f7122d8a75fb2b53dd2c7f2159505f6b3cb8668
Joren Vansteenkiste <joren.vansteenkiste@ugent.be>
2024-08-13 12:21:28 +0200

    Chart documentation

--- src/main/java/irealpro/Chart.java
+++ src/main/java/irealpro/Chart.java
@@ -9,6 +9,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+/**
+ * represents an IRealPro chord chart
+ */
 public class Chart {
 
     private DuplicateFileResolver resolver;
@@ -16,6 +19,12 @@ public class Chart {
     private final Song song;
     private final Part part;
 
+    /**
+     * @param song the song where the chart is a part of
+     *             used for metadata
+     * @param part the chart will be created from this part
+     *             used for constructing the body of the chart
+     */
     public Chart(Song song, Part part) {
         this.song = song;
         this.part = part;
@@ -23,43 +32,84 @@ public class Chart {
         this.resolver = () -> false;
     }
 
+    /**
+     * set functional interface DuplicateFileResolver for resolving conflicts when trying to export to a file
+     * @param resolver implementation that needs to be used
+     */
     public void setDuplicateFileResolver(DuplicateFileResolver resolver) {
         this.resolver = resolver;
     }
 
+    /**
+     * export this chart to a .html file
+     * more info on IReal Pro file format: <a href="https://www.irealpro.com/ireal-pro-custom-chord-chart-protocol">format</a>
+     * @param path place where the file should be saved, is not used currently
+     */
+    // TODO: use path
     public void export(String path) {
         String fileName = song.getTitle() + ".html";
         File export = new File(fileName);
+
         if (Files.exists(Path.of(export.getAbsolutePath()))) {
-            if (resolver.override()) {
-                System.out.println("overriding existing file: " + export.getAbsolutePath());
-                if (export.delete()) {
-                    export(path);
-                }
-            } else {
-                System.out.println("file already exists: " + export.getAbsolutePath());
-            }
+            resolveDuplicate(path, export);
         } else {
-            try (FileWriter writer = new FileWriter(fileName)) {
-                writer.write("<a href=\"" + this + "\">" + song.getTitle() + "</a>");
-                System.out.println("export success");
-            } catch (IOException e) {
-                System.out.println("could not create file");
-            }
+            write(fileName);
         }
     }
 
+    /**
+     * write the chart to a file
+     * @param fileName the filename to write to
+     */
+    private void write(String fileName) {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write("<a href=\"" + this + "\">" + song.getTitle() + "</a>");
+            System.out.println("export success");
+        } catch (IOException e) {
+            System.out.println("could not create file");
+        }
+    }
+
+    /**
+     * calls the resolver.override() method to check what to do when the file it wants to export to, already exists
+     * if this returns true, the already existing file will be overridden
+     * @param path place to export to, not used currently
+     * @param export the already existing file
+     */
+    private void resolveDuplicate(String path, File export) {
+        if (resolver.override()) {
+            System.out.println("overriding existing file: " + export.getAbsolutePath());
+            if (export.delete()) {
+                export(path);
+            }
+        } else {
+            System.out.println("file already exists: " + export.getAbsolutePath());
+        }
+    }
+
+    /**
+     * @return the body of the html tag in IReal Pro file format
+     */
     @Override
     public String toString() {
-        // more info on iReal Pro file format: https://www.irealpro.com/ireal-pro-custom-chord-chart-protocol
-        StringBuilder builder = new StringBuilder();
-        // add song metadata
-        builder.append("irealbook://").append(song.getTitle()).append("=")
-                .append(song.getComposer().lastName()).append(" ").append(song.getComposer().firstName()).append("=")
-                .append(song.getStyle()).append("=")
-                .append(part.getKey()).append("=n=");
+        return buildMetaData() + buildBody();
+    }
 
-        // add each measure to the url
+    /**
+     * @return the header for the custom IReal Pro URL containing its metadata
+     */
+    private String buildMetaData() {
+        return "irealbook://" + song.getTitle() + "=" +
+                song.getComposer().lastName() + " " + song.getComposer().firstName() + "=" +
+                song.getStyle() + "=" +
+                part.getKey() + "=n=";
+    }
+
+    /**
+     * @return the chart itself containing the chord progression in the correct IReal Pro format
+     */
+    private String buildBody() {
+        StringBuilder builder = new StringBuilder();
         Time time = null;
         for (Measure measure : part.getMeasures()) {
             if (measure.isImplicit()) continue;
@@ -88,7 +138,6 @@ public class Chart {
                 builder.append(measure.getEndBarLine().symbol(BarLinePosition.END));
             }
         }
-
         return builder.toString();
     }
 }
