866e655e3ad87ddad984dd5d7415fbd8550df960
Joren Vansteenkiste <joren.vansteenkiste@ugent.be>
2024-08-15 10:45:20 +0200

    documentation

--- src/main/java/core/exportable/DuplicateFileResolver.java
+++ src/main/java/core/exportable/DuplicateFileResolver.java
@@ -2,5 +2,9 @@ package core.exportable;
 
 @FunctionalInterface
 public interface DuplicateFileResolver {
+    /**
+     * function for determining if a file should be overridden when exporting an Exportable
+     * @return true if the file should be overridden, false if not
+     */
     boolean resolve();
 }
